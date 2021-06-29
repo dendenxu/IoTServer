803429d50819627f8ea54ed01a68cd941899600e
neon-cubes <56295612@qq.com>
2021-06-29 19:04:57 +0800

    feat: better populate_db.sh

--- populate_db.sh
+++ populate_db.sh
@@ -11,16 +11,22 @@ NoColor=$(tput sgr0)
 DB_FILE_BASE=${DB_FILE_BASE:-"mongodb_backup_20210629"}
 DB_FILE_EXT=${DB_FILE_EXT:-".tar.gz"}
 
-echo "${Yellow}DB_POPULATOR: set variable DB_FILE_BASE to change backup file base name, currently using ${DB_FILE_BASE}${NoColor}"
-echo "${Yellow}Checking mongodb status...${NoColor}"
-mongo --eval "db.getMongo().getDBNames().indexOf('iotserver');" > /dev/null
+echo -e "${Yellow}$0:\nset variable DB_FILE_BASE to change backup file base name, currently using ${DB_FILE_BASE}${NoColor}\n"
+echo -e "${Yellow}Checking mongodb status...${NoColor}\n"
+INDEX_IOTSERVER=$(mongo --quiet --eval "db.getMongo().getDBNames().indexOf('iotserver');")
 
-if [ $? -eq 0 ]; then
-    echo "${Red}You've already populated the database iotserver! Exiting...${NoColor}"
+echo -e "${Yellow}If you later want a fresh restart, you can use"
+echo -e "${Blue}mongo iotserver --eval "'"db.dropDatabase()"'
+echo -e "${Yellow}to remove the current iotserver database and run this script again${NoColor}\n"
+
+if [ "${INDEX_IOTSERVER}" != "-1" ]; then
+    echo -e "${Red}You've already populated the database iotserver! Exiting...${NoColor}"
     exit 1;
 fi
 
-echo "${Yellow}Extracting database backup from ${DB_FILE_BASE}${DB_FILE_EXT} ...${NoColor}"
+echo -e "${Yellow}Extracting database backup from ${DB_FILE_BASE}${DB_FILE_EXT} ...${NoColor}\n"
 tar -xvf "${DB_FILE_BASE}${DB_FILE_EXT}"
-echo "${Yellow}Using mongorestore to restore the database...${NoColor}"
+echo -e "${Yellow}Using mongorestore to restore the database...${NoColor}\n"
 mongorestore "${DB_FILE_BASE}"
+
+echo -e "${Green}All OK, you can run your iotserver now.${NoColor}\n"
